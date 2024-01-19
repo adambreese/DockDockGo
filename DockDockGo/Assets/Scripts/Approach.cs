d332d47bfaf0ae3aeda1d689a65996af2f824547
Adam Breese <adambreese@pm.me>
2024-01-19 16:13:17 -0500

    player prefabs and approach script
    - created player prefabs so player can be different shapes
    -- added playercontroller script to each prefab
    -- added rigidbodies and colliders to each prefab
    
    - Approach script: update function now causes obstacle to approach player with a public speed

--- DockDockGo/Assets/Scripts/Approach.cs
+++ DockDockGo/Assets/Scripts/Approach.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Approach : MonoBehaviour
 {
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Approach : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
